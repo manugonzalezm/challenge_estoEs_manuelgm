b67b229aafda46a77430c5bcd747a6290fb43b37
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-07-21 17:30:36 -0300

    Setting up the Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import TaskListContainer from './components/display/TaskListContainer';
+import TaskForm from './components/TaskForm';
 
 function App() {
+
   return (
-    <div>
-      <h1>Hola Mundo</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/">
+          <Route index element={<TaskListContainer />} />
+          <Route path="tasks">
+            <Route path=":taskId" element={<TaskForm />} />
+            <Route path="new" element={<TaskForm />} />
+            <Route index element={<TaskForm />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
