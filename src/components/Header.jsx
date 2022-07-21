1729c0b1006dc49141bdeb39561081c808ce316f
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-07-21 17:22:18 -0300

    Preliminar Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import{ Navbar, Container } from 'react-bootstrap';
+import logo from '../assets/images/logo.png'; 
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <Navbar className="border-bottom">
+            <Container>
+                <Navbar.Brand > {/* Despues poner link al home */}
+                    <img
+                        src={logo}
+                        className="d-inline-block align-top"
+                        alt="Estoes logo"
+                    />
+                </Navbar.Brand>
+            </Container>
+        </Navbar>
     )
 }
 
