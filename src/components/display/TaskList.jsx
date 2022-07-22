b7a6606c1506a9769cf0efd0135f88b490a6bc89
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-07-21 22:22:59 -0300

    Tasks list firestore logic and mapping

--- src/components/display/TaskList.jsx
+++ src/components/display/TaskList.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import {
+    Table,
+    Button
+} from 'react-bootstrap';
 
-const TaskList = () => {
+const TaskList = (props) => {
+    const tasks = useSelector((state) => state.tasks)
+
+    const { list } = props
+
     return (
-        <div>TaskList</div>
+        <Table>
+            <thead className="bg-light">
+                <tr>
+                    <th>Project info</th>
+                    <th>Project Manager</th>
+                    <th>Assigned to</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {list.map(t => (
+                    <tr key={t.id}>
+                        <td>{t.title}</td>
+                        <td>{t.project_manager}</td>
+                        <td>{t.assigned_to}</td>
+                        <td>{t.status}</td>
+                        <td>
+                            <Button variant="outline-light" className="text-dark">
+                                Icon ...
+                            </Button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
     )
 }
 
--- src/components/display/TaskListContainer.jsx
+++ src/components/display/TaskListContainer.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from 'react-redux';
+import { 
+    getFirestore, 
+    collection, 
+    getDocs, 
+    deleteDoc 
+} from 'firebase/firestore';
+import {
+    Container
+} from 'react-bootstrap';
+import TaskList from "./TaskList";
+
+import appFirebase from '../../firebase';
+const db = getFirestore(appFirebase)
 
 const TaskListContainer = () => {
-    return <div>TaskListContainer</div>;
+    const [list, setList] = useState([])
+
+    useEffect(() => {
+        const getList = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, "tasks"))
+                const docs = []
+                querySnapshot.forEach((doc) => {
+                    docs.push({ ...doc.data(), id: doc.id })
+                })
+
+                setList(docs)
+                console.log(docs)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        if (list.length === 0) {
+            getList()
+        }
+    }, [list])
+    //console.log(list)
+
+    return (
+        <div className="bg-light pt-5">
+            <Container className="border bg-white shadow px-0">
+                <TaskList list={list} />
+            </Container>
+        </div>
+    );
 };
 
 export default TaskListContainer;
