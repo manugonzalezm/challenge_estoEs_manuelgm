229fb5ce31e42aaa0c9618536ea1772ccb362c6b
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-07-22 12:01:33 -0300

    Avatars and styling

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+export const Logo = require('./logo.png');
+export const Gabriela_Fuentes = require('./avatar_Gabriela Fuentes.png');
+export const Ignacio_Truffa = require('./avatar_Ignacio Truffa.png');
+export const Juan_Perez = require('./avatar_Juan Perez.png');
+export const Manuel_Gonzalez = require('./avatar_Manuel Gonzalez.png');
+export const Pedro_Sanchez = require('./avatar_Pedro Sanchez.png');
+export const Walt_Cosani = require('./avatar_Walt Cosani.png');
